50019c1c0d0ac7ea28b6f7afe950f0c6b6614afc
jibananando <jibananando.pramanik@gmail.com>
2022-11-22 04:00:15 +0600

    complated modal

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,9 +1,11 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import BookingModal from '../BookingModal/BookingModal';
 import AppointmentOption from './AppointmentOption';
 
 const AvailableAppointments = ({ selectedDate }) => {
     const [appointmentOptions, setAppointmentOptions] = useState([]);
+    const [treatment, setTreatment] = useState(null);
 
 
     useEffect(() => {
@@ -20,9 +22,17 @@ const AvailableAppointments = ({ selectedDate }) => {
                     appointmentOptions.map(option => <AppointmentOption
                         key={option._id}
                         appointmentOption={option}
+                        setTreatment={setTreatment}
                     ></AppointmentOption>)
                 }
             </div>
+            {
+                treatment &&
+                <BookingModal
+                    selectedDate={selectedDate}
+                    treatment={treatment}
+                ></BookingModal>
+            }
         </section>
     );
 };
--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -1,50 +1,22 @@
 import { format } from 'date-fns';
 import React from 'react';
 
-const BookingModal = ({ treatment, setTreatment, selectedDate }) => {
-    // treatment is just another name of appointmentOptions with name, slots, _id
+const BookingModal = ({ treatment, selectedDate }) => {
     const { name, slots } = treatment;
     const date = format(selectedDate, 'PP');
-
-    const handleBooking = event => {
-        event.preventDefault();
-        const form = event.target;
-        const slot = form.slot.value;
-        const name = form.name.value;
-        const email = form.email.value;
-        const phone = form.phone.value;
-        // [3, 4, 5].map((value, i) => console.log(value))
-        const booking = {
-            appointmentDate: date,
-            treatment: name,
-            patient: name,
-            slot,
-            email,
-            phone,
-        }
-
-        // TODO: send data to the server
-        // and once data is saved then close the modal 
-        // and display success toast
-        console.log(booking);
-        setTreatment(null);
-    }
-
     return (
-        <>
+        <div>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
             <div className="modal">
                 <div className="modal-box relative">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold">{name}</h3>
-                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-10'>
+                    <form className='grid grid-cols-1 gap-3 mt-10'>
                         <input type="text" disabled value={date} className="input w-full input-bordered " />
-                        <select name="slot" className="select select-bordered w-full">
+                        <select className="select select-bordered w-full">
+
                             {
-                                slots.map((slot, i) => <option
-                                    value={slot}
-                                    key={i}
-                                >{slot}</option>)
+                                slots.map(slot => <option value={slot}>{slot}</option>)
                             }
                         </select>
                         <input name="name" type="text" placeholder="Your Name" className="input w-full input-bordered" />
@@ -55,7 +27,7 @@ const BookingModal = ({ treatment, setTreatment, selectedDate }) => {
                     </form>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
